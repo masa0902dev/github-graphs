618f4de5d6669510f416c5d320c6222099f8bd08
masa0902dev <masa0902dev@gmail.com>
2024-05-05 01:09:31 +0900

    feat: add count_contribution.js

--- count_contribution.js
+++ count_contribution.js
@@ -5,7 +5,9 @@ async function fetchContributions(endpoint) {
         const data = await response.json();
 
         // コントリビューション数を計算
-        const contributions = data.filter((event) => event.type === "PushEvent").length;
+        // const contributions = data.filter((event) => event.type === "PushEvent").length;
+        // const contributions = data.filter((event) => event.type === "PushEvent");
+        const contributions = data;
 
         return contributions;
     } catch (error) {
@@ -15,36 +17,37 @@ async function fetchContributions(endpoint) {
 }
 
 
-// input, outputフィールド
+
 const inputField = document.querySelector("#user-name");
 const outputField = document.querySelector("#user-count");
-// 実行ボタン
 const processBtn = document.querySelector("#count-contribution");
 
 // inputでEnterが押されたらボタンを押す
-document.querySelector("#user-name").addEventListener("keypress", (e) => {
+inputField.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         processBtn.click();
     }
 });
 
 processBtn.addEventListener("click", async () => {
-    
-    // GitHubのユーザー名
     const username = inputField.value;
-    // GitHub APIエンドポイント
-    const endpoint = `https://api.github.com/users/${username}/events`;
+    // const endpoint = `https://api.github.com/users/${username}/events`;
+    // const endpoint = `https://api.github.com/users/${username}/repos`;
+    // const endpoint = `https://api.github.com/${username}/repos`;
+    // const endpoint = `https://api.github.com/users/${username}/repos`;
+    const endpoint = `https://api.github.com/repos/${username}/csv-control-nodejs/activity`;
+
+
 
     // コントリビューション数を取得して出力
     let contributions = await fetchContributions(endpoint);
     if (contributions !== null) {
-        console.log(`ユーザー '${username}' のコントリビューション数は ${contributions} 回です。`);
+        console.log(contributions);
+        // console.log(`ユーザー '${username}' のコントリビューション数は ${contributions} 回です。`);
         outputField.value = contributions + " contributions";
     } else {
         console.log("コントリビューション数を取得できませんでした。")
         outputField.value = "Error";
         outputField.style.color = "red";
     }
-
-    // 出力
 });
