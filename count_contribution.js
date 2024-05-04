016f1085b0302fb41e82e2ee4eaf5240e95caa86
masa0902dev <masa0902dev@gmail.com>
2024-05-04 23:29:23 +0900

    feat: add most simplest contribution display

--- count_contribution.js
+++ count_contribution.js
@@ -1,5 +1,3 @@
-
-
 // GitHub APIからデータを取得する関数
 async function fetchContributions(endpoint) {
     try {
@@ -17,12 +15,23 @@ async function fetchContributions(endpoint) {
 }
 
 
+// input, outputフィールド
+const inputField = document.querySelector("#user-name");
+const outputField = document.querySelector("#user-count");
 // 実行ボタン
 const processBtn = document.querySelector("#count-contribution");
 
+// inputでEnterが押されたらボタンを押す
+document.querySelector("#user-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        processBtn.click();
+    }
+});
+
 processBtn.addEventListener("click", async () => {
+    
     // GitHubのユーザー名
-    const username = document.querySelector("#user-name").value;
+    const username = inputField.value;
     // GitHub APIエンドポイント
     const endpoint = `https://api.github.com/users/${username}/events`;
 
@@ -30,7 +39,12 @@ processBtn.addEventListener("click", async () => {
     let contributions = await fetchContributions(endpoint);
     if (contributions !== null) {
         console.log(`ユーザー '${username}' のコントリビューション数は ${contributions} 回です。`);
+        outputField.value = contributions + " contributions";
     } else {
-        console.log("コントリビューション数を取得できませんでした。");
+        console.log("コントリビューション数を取得できませんでした。")
+        outputField.value = "Error";
+        outputField.style.color = "red";
     }
+
+    // 出力
 });
