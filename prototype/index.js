08a8715e49062cc3db0cd851b342e76c9c8b69ca
masa0902dev <masa0902dev@gmail.com>
2024-05-06 13:25:49 +0900

    feat: add contribution graph style

--- prototype/index.js
+++ prototype/index.js
@@ -1,7 +1,7 @@
-const countContributionBtn = document.getElementById("countContribution");
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("count-contribution");
+    button.addEventListener("click", fetchContributions);
+});
 
 async function fetchContributions() {
     const userName = document.getElementById("username").value;
@@ -17,11 +17,11 @@ async function fetchContributions() {
     const data = await response.json();
     // // データを localStorage に保存
     // localStorage.setItem("data", JSON.stringify(data));
-    RenderContributionGraph(data);
+    RenderContributionGraph(data, period);
 }
 
 
-function RenderContributionGraph(data) {
+function RenderContributionGraph(data, period) {
     const result = document.getElementById("result");
     result.innerHTML = ""; // 古い内容をクリア
     const weeks = data.data.user.contributionsCollection.contributionCalendar.weeks;
@@ -34,42 +34,8 @@ function RenderContributionGraph(data) {
     // table
     const table = document.createElement("table");
     table.style.borderCollapse = "collapse";
-    // table.style.border = "1px solid black";
 
-    // for (let i = 0; i < 7; i++) { // 7日ごとの行
-    //     const row = document.createElement("tr");
-    //     for (let j = 0; j < 52; j++) { // 52週ごとの列
-    //         const index = i * 52 + j;
-    //         const cell = document.createElement("td");
-    //         cell.style.width = "2rem";
-    //         cell.style.height = "2rem";
-    //         cell.style.border = "1px solid black";
-
-    //         const num = contributions[index];
-    //         if (num === undefined) cell.style.backgroundColor = "white";
-    //         else if (num === 0) cell.style.backgroundColor = "white";
-    //         else if (num < 5) cell.style.backgroundColor = "lightgreen";
-    //         else if (num < 10) cell.style.backgroundColor = "green";
-    //         else if (num < 20) cell.style.backgroundColor = "darkgreen";
-    //         else {
-    //             console.log("num:", num);
-    //             cell.style.backgroundColor = "black";
-    //         }
-
-    //         // cell.style.backgroundColor = contributions[index] === 0 ? "white" : "green";
-    //         cell.textContent = dates[index];
-    //         cell.style.fontSize = "8px";
-    //         row.appendChild(cell);
-    //     }
-    //     table.appendChild(row);
-    // }
-    // result.appendChild(table);
-
-
-
-    // 6months or 1year
     const numberOfCols = period === "6" ? 27 : 53;
-
     // make empty table
     for (let i = 0; i < 7; i++) {
         const row = document.createElement("tr");
@@ -77,24 +43,77 @@ function RenderContributionGraph(data) {
             const cell = document.createElement("td");
             cell.style.width = "20px";
             cell.style.height = "20px";
-            cell.style.border = "1px solid black";
-            // cell.style.backgroundColor = "white";
+            cell.style.borderRadius = "2px";
+            // cell.style.background = "white";
             row.appendChild(cell);
         }
         table.appendChild(row);
     }
     result.appendChild(table);
 
-    // fill table
-    // 1. check the first day of the week
-    const firstDate = new Date(dates[0]);
-    const firstDay = firstDate.getDay();
-    console.log("firstDate:", firstDate);
-    console.log("firstDay:", firstDay);
+    // 1. check and fill the lack of days in the first week
+    const firstDay = new Date(dates[0]).getDay();
+    let daysCount = firstDay;
+    let weekCount = 0;
+    for (let i = 0; i < firstDay; i++) {
+        let cell = table.rows[i].cells[0];
+        cell.style.background = getColor(0);
+    }
+    // 2. fill the table
+    for (let i = 0; i < dates.length; i++) {
+        const dateInstance = new Date(dates[i]);
+        const day = dateInstance.getDay();
 
+        let cell = table.rows[day].cells[weekCount];
+        cell.style.background = getColor(contributions[i]);
+        console.log(cell, contributions[i]);
+        // cell.textContent = `${day}: ${contributions[i]}`;
 
-    // 2. put following days
+        daysCount++;
+        if (daysCount === 7) {
+            weekCount++;
+            daysCount = 0;
+        }
+    }
+    // 3. fill the lack of days in the last week
+    for (let i = daysCount; i < 7; i++) {
+        let cell = table.rows[i].cells[weekCount];
+        cell.style.background = getColor(0);
+    }
 }
 
+
+function getColor(number) {
+    const colors = [
+        "rgb(0 0 0 / .03)", //gray (empty or 0)
+        "rgb(0 255 0 / .33)", //green
+        "rgb(0 255 0 / .66)",
+        "rgb(0 255 0)",
+        "rgb(255 255 0 / .33)", //yellow
+        "rgb(255 255 0 / .66)",
+        "rgb(255 255 0)",
+        "rgb(255 165 0 / .33)", //orange
+        "rgb(255 165 0 / .66)",
+        "rgb(255 165 0)",
+        "rgb(255 0 0 / .33)", //red
+        "rgb(255 0 0 / .66)",
+        "rgb(255 0 0)",
+        "linear-gradient(135deg, rgb(0 255 255 / .8), rgb(255 0 255 / .8), rgb(255 255 0 / .8)" //rainbow
+
+    ];
+
+
+    for (let i = 0; i < colors.length; i++) {
+        if (number === undefined || number === 0) {
+            return colors[0];
+        } else if (number < i*5  && number >= (i-1)*5) {
+            return colors[i];
+        }
+
+        if (number >= 60) {
+            return colors[colors.length - 1];
+        }
+    }
+}
 
 
